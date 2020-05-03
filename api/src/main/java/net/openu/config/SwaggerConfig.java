3ab15f967dfd47648edc4e8980a9737fde1b2a2e
byungnambeak <iopenu@gmail.com>
2020-05-03 23:12:12 +0900

    [swagger] config

---
+++ api/src/main/java/net/openu/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package net.openu.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by iopenu@gmail.com on 2020/05/03
+ * Github : https://github.com/bnbaek
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
+
+}
